ce3ce549f35b7c056cc7fe690cfa0358dee050e0
BuildTools <unconfigured@null.spigotmc.org>
2023-11-17 15:40:17 -0700

    wip

--- scenes/hud/hud.gd
+++ scenes/hud/hud.gd
@@ -3,26 +3,42 @@ extends CanvasLayer
 signal start_game
 signal player_ready
 
-func end_game():
+const TITLE = "Sucks to Suck"
+const END_TITLE = "I'm sure you suck at other stuff"
+
+func game_over():
 	# Display a game over message
-	$Root/EndLabel.text = "Sucks to suck"
+	$Root/EndLabel.text = TITLE
 	$Root/ScoreLabel.hide()
 	$Root/EndLabel.show()
 	await get_tree().create_timer(1.5).timeout
 	$Root/StartButton.show()
+	
+func end_game():
+	# Display a game over message
+	$Root/EndLabel.text = END_TITLE
+	$Root/ScoreLabel.hide()
+	$Root/EndLabel.show()
+	await get_tree().create_timer(1.5).timeout
+	$Root/StartButton.show()
+	$Root/EndLabel.text = TITLE
 
 func _on_start_button_pressed():
 	$Root/StartButton.hide()
 	$Root/EndLabel.hide()
 	
 	# Display a start game message
-	$Root/ReadyLabel.show()
+	#$Root/ReadyLabel.show()
+	
 	player_ready.emit()
-	await get_tree().create_timer(1.5).timeout
-	$Root/ReadyLabel.hide()
+	
+	#await get_tree().create_timer(1.5).timeout
+	
+	#$Root/ReadyLabel.hide()
+	
 	$Root/ScoreLabel.show()
 	
-	start_game.emit()
+
 
 func hit(lives: int):
 	$Root/ScoreLabel.text = str(lives)
@@ -30,4 +46,16 @@ func hit(lives: int):
 func level_complete():
 	$Root/EndLabel.text = "I guess you don't suck"
 	$Root/EndLabel.show()
+	await get_tree().create_timer(1.5).timeout
 	
+func level_start(n: int, name: String):
+	$Root/LevelNumberLabel.text = str(n)
+	$Root/EndLabel.text = name 
+	
+	$Root/EndLabel.show()
+	$Root/LevelNumberLabel.show()
+	await get_tree().create_timer(2.5).timeout
+	$Root/EndLabel.hide()
+	$Root/LevelNumberLabel.hide()
+	
+	start_game.emit()
