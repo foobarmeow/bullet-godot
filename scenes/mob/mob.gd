33e60d8c5b6bb62efbfdbc9be3e4f961e7a60d1e
BuildTools <unconfigured@null.spigotmc.org>
2023-11-16 17:53:06 -0700

    more wip

--- scenes/mob/mob.gd
+++ scenes/mob/mob.gd
@@ -1,11 +1,10 @@
-extends RigidBody2D	
+extends Area2D
+
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
-	# set as invisible to start
-	visible = false
-	gravity_scale = 0
-	print("FUCK")
+	pass # Replace with function body.
+
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
