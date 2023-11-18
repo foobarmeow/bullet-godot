53cefe8bf669a759d8499abf36d26380bf904fb1
BuildTools <unconfigured@null.spigotmc.org>
2023-11-17 22:37:40 -0700

    mover animated working

--- scenes/mobs/mover/mover.gd
+++ scenes/mobs/mover/mover.gd
@@ -1,36 +1,48 @@
-extends Node2D
+extends PathFollow2D
 
-@export var speed: int = 1
+@export var speed: float = .01
 @export var offset_from_player: int = 25
 
 const PROGRAM_RANDOM = "random"
+const PROGRAM_PATH = "path"
 
 var program: String
 var player: Node2D
 var dir: Vector2
 var target: Vector2
 
+
 func _ready():
-	start(PROGRAM_RANDOM, get_node("../Player"))
+	#start(PROGRAM_RANDOM, get_node("../Player"))
+	start_path(get_node("../../Player"))
+
+func start_path(p):
+	program = PROGRAM_PATH
+	player = p
 
 func start(prgm: String, p):
 	program = prgm
 	player = p
-	
 	if player == null:
 		return
-	
 	target = get_random_position()
 
 func _process(delta):
-	var diff = position - target
-	# some threshold here would be good
-	if diff.length() < 5:
-		target = get_random_position()
-		return
-	var v = diff * speed
-	translate(v.rotated(rotation) * delta)
-	
+	$AnimatedSprite2D.play()
+	match program:
+		PROGRAM_RANDOM:
+			var diff = position - target
+			# some threshold here would be good
+			if diff.length() < 5:
+				target = get_random_position()
+				return
+			var v = diff * speed
+			translate(v.rotated(rotation) * delta)
+		PROGRAM_PATH:
+			var i = speed * delta
+			print(i, delta)
+			progress_ratio += i
+			
 func get_random_position(offset: int = 0):
 	if player == null:
 		return
