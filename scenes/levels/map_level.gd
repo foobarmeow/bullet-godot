8c57d869502a13cb6b23ff4d4873d3a6d081aacd
BuildTools <unconfigured@null.spigotmc.org>
2023-11-19 11:37:04 -0700

    enemy alerts

--- scenes/levels/map_level.gd
+++ scenes/levels/map_level.gd
@@ -4,8 +4,8 @@ extends Node2D
 # Called when the node enters the scene tree for the first time.
 func _ready():
 	for c in get_children():
-		if c.name == "Mover":
-			c.begin($Player)
+		if c is Mover:
+			c.player = $Player
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
