0b9bc96d52fc0b2f53e49dcf1ba2e745604c3f66
caleb <caleb@calebs-mbp.lan>
2023-11-19 16:03:05 -0700

    Before input change

--- scenes/levels/map_level.gd
+++ scenes/levels/map_level.gd
@@ -3,10 +3,16 @@ extends Node2D
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
-	for c in get_children():
-		if c is Mover:
-			c.player = $Player
-			c.begin()
+	start_movers(get_children())
+
+func start_movers(nodes: Array[Node]):
+	for n in nodes:
+		if n.get_child_count() > 0:
+			start_movers(n.get_children())
+		if n is Mover:
+			n.player = $Player
+			n.begin()
+	
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
