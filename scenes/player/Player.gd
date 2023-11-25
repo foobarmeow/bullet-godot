7bb20bb4fa0e445f21b40cab3fd75903be66f409
BuildTools <unconfigured@null.spigotmc.org>
2023-11-24 17:39:16 -0700

    Parry closer

--- scenes/player/Player.gd
+++ scenes/player/Player.gd
@@ -3,6 +3,8 @@ class_name Player extends CharacterBody2D
 signal health_updated
 
 @export var speed = 500
+@export var acceleration = 50
+@export var top_speed = 800
 @export var steps_to_accel: float = 3
 @export var steps_to_decel: float = 2
 @export var initial_health = 100
@@ -14,6 +16,7 @@ const TIME_PER_STEP: float = 0.2
 var screen_size = Vector2.ZERO
 var initial_modulate: Color
 var parryables: Array[Node2D]
+var initial_speed: int
 
 
 var health: int = initial_health:
@@ -21,10 +24,6 @@ var health: int = initial_health:
 		health = v
 		health_updated.emit(health)
 	
-var local_speed: int = 0
-var walk_delta: float = 0
-var walk_steps: int = 0
-var last_velocity: Vector2
 var sprite: AnimatedSprite2D
 var taking_damage: bool
 var bounced: bool = false
@@ -40,6 +39,8 @@ func _draw():
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
+	initial_speed = speed
+	
 	#$Parry.process_mode = Node.PROCESS_MODE_DISABLED
 	if $AnimatedSprite2D == null:
 		print_debug("no sprite bruh")
@@ -99,59 +100,21 @@ func handle_movement(delta: float):
 		return
 	
 	if Input.is_action_pressed("move_right"):
-		if sprite != null:
-			sprite.flip_h = false
+		sprite.flip_h = false
 	if Input.is_action_pressed("move_left"):
-		if sprite != null:
-			sprite.flip_h = true
+		sprite.flip_h = true
 		
 	var input_direction = Input.get_vector("move_left", "move_right", "move_up", "move_down")
-	velocity = input_direction
-
-	#if Input.is_action_just_pressed("move_right") || Input.is_action_just_pressed("move_left"):
-		#walk_steps = 0	
+	velocity = input_direction * speed
 	
-	# If our intent is to move forward
-	if velocity.length() != 0:
-		
-		# immediately start walking instead of letting an 
-		# empty step pass
-		if walk_delta == 0:
-			walk_steps = 1
-			
-		# increment steps if enough time has passed
-		if walk_delta >= TIME_PER_STEP:
-			walk_steps = clamp(walk_steps+1, 0, steps_to_accel)
-			walk_delta = 0
-			
-		# accelerate if we've taken few enough steps
-		if walk_steps <= steps_to_accel:
-			local_speed = (speed/steps_to_accel) * walk_steps 
-		
-		if sprite != null:
-			sprite.play("walk")
-	elif walk_steps > 0:
-		# If our velocity is zero but walk_steps > 0 
-		# we just stopped moving because we haven't reset
-		# walk_steps
-		if walk_delta >= TIME_PER_STEP:
-			walk_steps = clamp(walk_steps-1, 0, steps_to_decel)
-			walk_delta = 0
-			
-		var decel_amount = speed/steps_to_decel
-		local_speed -= decel_amount
-		velocity = last_velocity
-		
-	local_speed = clamp(local_speed, 0, speed)	
-	walk_delta += delta
-	velocity = velocity.normalized() * local_speed
+	if speed < top_speed:
+		speed += acceleration
+	
+	sprite.play("walk")
 
 	if velocity.length() == 0:
-		if sprite != null:
-			sprite.play("idle")
-		#local_speed = 0
-		walk_delta = 0
-		walk_steps = 0
+		sprite.play("idle")
+		speed = initial_speed
 		
 	var collision = move_and_collide(velocity * delta)
 	if collision:
@@ -167,9 +130,6 @@ func handle_movement(delta: float):
 			velocity = velocity.bounce(collision.get_normal())
 		else:
 			velocity = velocity.slide(collision.get_normal())
-	#position += velocity * delta
-	#position = position.clamp(Vector2.ZERO, screen_size)
-	last_velocity = velocity
 	
 # Called by bullets that detect collision with player
 func take_damage(d: int):
