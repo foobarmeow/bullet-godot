7d3284242db17bb282cf626612b652957f95f063
BuildTools <unconfigured@null.spigotmc.org>
2023-12-05 21:09:21 -0700

    Move the action interception up to GUI (which doesn't get paused)

--- scenes/components/dialog_box.gd
+++ scenes/components/dialog_box.gd
@@ -5,7 +5,12 @@ enum BoxType {ACTION, DIALOG}
 @export var type: BoxType = BoxType.DIALOG
 
 var dialog_dict = {
-	"action_drink": ["Drink!\n(space)"],
+	"action_drink": "Drink!\n(space)",
+	"intro": [
+		"My boy...it's time to drink.",
+		"From the well...",
+		"Go find it, get that drank son..."
+	],
 	"drink_success": [
 		"Nice you drank, that's sick.",	
 	 	"Now get back to me.",
@@ -22,15 +27,23 @@ func _ready():
 			SignalBus.connect("display_dialog", _on_display_dialog)
 			SignalBus.connect("hide_dialog", _on_hide_dialog)
 		BoxType.ACTION:
-			SignalBus.connect("display_action", _on_display_dialog)
+			SignalBus.connect("display_action", _on_display_action)
 			SignalBus.connect("hide_action", _on_hide_dialog)
 			SignalBus.connect("input_action", _on_input_action)
+
+
+func _on_display_action(text_key: String):
+	if !is_visible:
+		$Label.text = dialog_dict[text_key]
+		$AnimationPlayer.play("show")
+		is_visible = true
 
 
 func _on_display_dialog(text_key: String):
 	if in_progress:
 		next_line()
 	elif text_key != "advance_text":
+		get_tree().paused = true
 		in_progress = true
 		selected_text = dialog_dict[text_key].duplicate()
 		$AnimationPlayer.play("show")
@@ -47,6 +60,7 @@ func finish():
 	$AnimationPlayer.play_backwards("show")
 	$Label.text = ""
 	in_progress = false
+	get_tree().paused = false
 	
 func _on_hide_dialog():
 	if is_visible:
