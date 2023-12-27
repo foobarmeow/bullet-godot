0a4d57a7964c3134f466347d87e0433b796c3829
foobarmeow <caleb.aaron.davis@gmail.com>
2023-12-27 12:52:28 -0700

    Parry almost there

--- scenes/kill_light.gd
+++ scenes/kill_light.gd
@@ -1,30 +1,15 @@
 @tool
 extends Node2D
 
-signal parry_recharge
-
-var player
-
-# Called when the node enters the scene tree for the first time.
-func _ready():
-	#$AnimationPlayer.play("idle")
-	pass # Replace with function body.
+signal parry_recharged
 
 
-# Called every frame. 'delta' is the elapsed time since the previous frame.
-func _process(delta):
-	pass
-
-
-
-func _on_player_parry(p: Player):
+func activate():
 	$AnimationPlayer.play("parry_activate")
-	player = player
-
 
 func _on_animation_player_animation_finished(anim_name):
 	match anim_name:
 		"parry_activate":
 			$AnimationPlayer.play("parry_recharge")
 		"parry_recharge":
-			player.parried = false
+			parry_recharged.emit()
