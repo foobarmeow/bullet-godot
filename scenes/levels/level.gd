2b32185e4b722a465dc24b49e8e1dc9fa312f819
foobarmeow <caleb.aaron.davis@gmail.com>
2023-12-27 12:52:16 -0700

    wip

--- scenes/levels/level.gd
+++ scenes/levels/level.gd
@@ -2,8 +2,22 @@ extends Node2D
 
 signal level_complete
 
+@export var level_name: String
+
 var player: Area2D
 var hud: CanvasLayer
+
+
+# so if we run the scene alone, we can
+func _ready():
+	for wave in get_children():
+		if wave.name.to_lower() == "player":
+			player = wave
+			
+	if player != null:
+		_on_player_restart()
+		start(player, null)
+	
 
 func start(p: Area2D, h: CanvasLayer):
 	player = p
@@ -25,7 +39,8 @@ func start(p: Area2D, h: CanvasLayer):
 	level_complete.emit(player.lives > 0)
 	
 func _on_player_hit(lives: int):
-	hud.hit(lives)
+	if hud != null:
+		hud.hit(lives)
 		
 func _on_player_restart():
 	player.position = $PlayerSpawnPosition.position
