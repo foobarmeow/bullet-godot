4bd06f4d86970333c6db595a49042e7dad8f979d
foobarmeow <caleb.aaron.davis@gmail.com>
2023-12-27 12:52:29 -0700

    Invuln should use animation player not tweens

--- scenes/components/DamageManager.gd
+++ scenes/components/DamageManager.gd
@@ -46,36 +46,15 @@ func take_damage(parent: Node2D, enemy: Node2D, dmg: int):
 	damage_animate(parent)
 	
 
-var initial_color: Color
+
 var tweens: Array[Tween]
 func damage_animate(parent: Node2D):
-	var sprite = parent.get_node("AnimatedSprite2D")
-	if !sprite:
-		print_debug("no sprite to modulate when taking damage! fuck!")
-		return
-		
-	if !initial_color:
-		initial_color = sprite.self_modulate
-		
-		
 	for t in tweens:
 		t.kill()
-		
-	if len(tweens) > 0:
-		# Reset the modulate in case 
-		# we killed the tween modulating it
-		sprite.self_modulate = initial_color
-		
 	tweens = []
 		
 	var initial_position = parent.position
-	var initial_color = parent.modulate
-	var color_tween = get_tree().create_tween()
-	tweens.append(color_tween)
-	
-	#color_tween.tween_property(sprite, "self_modulate", Color.RED, .05)
-	#color_tween.tween_property(sprite, "self_modulate", initial_color, .05)
-	invuln_animation(sprite, color_tween)
+
 	
 	var tween = get_tree().create_tween()
 	tweens.append(tween)
@@ -85,19 +64,5 @@ func damage_animate(parent: Node2D):
 		tween.tween_property(parent, "position", p, .025)
 	# Back to initial position
 	tween.tween_property(parent, "position", initial_position, .025)
-	
-func invuln_animation(sprite: Node2D, color_tween: Tween):
-	if invuln_time <= 0:
-		return
-		
-	var total_invuln_steps = 10
-	var invuln_step = invuln_time / total_invuln_steps
-	
-	
-	var vis_color = sprite.self_modulate
-	var invis_color = Color(vis_color, 0.0)
-	
-	for i in total_invuln_steps/2:
-		color_tween.tween_property(sprite, "modulate", invis_color, invuln_step)
-		color_tween.tween_property(sprite, "modulate", vis_color, invuln_step)
+
 	
