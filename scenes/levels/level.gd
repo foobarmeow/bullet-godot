7c48728734a7ac7a5cb243032b14f84686463e48
foobarmeow <caleb.aaron.davis@gmail.com>
2023-12-27 12:52:16 -0700

    wip

--- scenes/levels/level.gd
+++ scenes/levels/level.gd
@@ -2,10 +2,32 @@ extends Node2D
 
 signal level_complete
 
-func start(p: Area2D):
+var player: Area2D
+var hud: CanvasLayer
+
+func start(p: Area2D, h: CanvasLayer):
+	player = p
+	hud = h
+
+	p.hit.connect(_on_player_hit)
+	
 	for wave in get_children():
+		if !wave.name.to_lower().begins_with("wave"):
+			continue
+		if player.lives == 0:
+			break
+		
+		wave.player_restart.connect(_on_player_restart)
 		wave.start(p)
-		print("wave startes")
 		await wave.wave_complete
-		print("wave done")
-	level_complete.emit()
+	
+	print("level complete", player.lives)
+	level_complete.emit(player.lives > 0)
+	
+func _on_player_hit(lives: int):
+	hud.hit(lives)
+		
+func _on_player_restart():
+	player.position = $PlayerSpawnPosition.position
+	player.start()
+
--- scenes/main/Main.gd
+++ scenes/main/Main.gd
@@ -1,70 +1,26 @@
 extends Node2D
 
-@export var mob_holder_scene: PackedScene
-@export var lives: int = 3
 @export var levels: Array
 
-var _lives = lives
-
-func _on_spawn_timer_timeout():
-	spawn()
-		
-func spawn():
-	var types = [
-		"square", 
-		"line",
-	]
-	
-	var n_per_type = {
-		"square": 8,
-		"line": 2
-	}
-	
-	var t = types[randi_range(0, 1)]
-	for i in n_per_type[t]:
-		var mob_holder = mob_holder_scene.instantiate()
-		add_child(mob_holder)
-		mob_holder.start(t, $Player)
-
-func _on_player_hit():
-	get_tree().call_group("mob", "queue_free")
-	$SpawnTimer.stop()
-	await $Player.blink()
-	$Player.position = $PlayerSpawnPosition.position
-	$Player.start()
-	
-	
-	_lives -= 1
-	if _lives > 0:
-		$HUD.hit(_lives)
-		
-		#spawn()
-		#$SpawnTimer.start()
-	else:
-		end_game()
 
 func end_game():
 	$Player.end()
 	$HUD.end_game()
-	_lives = lives
-	$SpawnTimer.stop()
-
 
 func _on_hud_start_game():
+	$Player.lives = $Player.initial_lives
 	var l1 = levels[0].instantiate()
 	l1.level_complete.connect(_on_level_complete)
 	add_child(l1)
 	
-	l1.start($Player)
-	return
+	l1.start($Player, $HUD)
+	$HUD.hit($Player.lives)
 	
-	spawn()
-	$SpawnTimer.start()
-	$HUD.hit(_lives)
-	
-func _on_level_complete():
-	$HUD.level_complete()
-
+func _on_level_complete(alive: bool):
+	if !alive:
+		end_game()
+	else:
+		$HUD.level_complete()
 
 func _on_hud_player_ready():
 	$Player.start()
