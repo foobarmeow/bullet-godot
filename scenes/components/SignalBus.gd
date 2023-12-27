1ee26faa92bd6cc536062ba4985447f2192035ee
foobarmeow <caleb.aaron.davis@gmail.com>
2023-12-27 12:52:37 -0700

    I did switch to a signalbus

---
+++ scenes/components/SignalBus.gd
@@ -0,0 +1,5 @@
+extends Node
+
+signal display_dialog(text_key: String)
+signal display_action(text_key: String)
+signal hide_dialog
--- scenes/components/dialog_box.gd
+++ scenes/components/dialog_box.gd
@@ -1,6 +1,25 @@
 extends Control
 
-func show_dialog(dialog: String):
+enum BoxType {ACTION, DIALOG}
+
+@export var type: BoxType = BoxType.DIALOG
+
+var dialog_dict = {
+	"action_drink": "Drink!\n(space)"
+}
+
+func _ready():
+	match type:
+		BoxType.DIALOG:
+			SignalBus.connect("display_dialog", _on_display_dialog)
+		BoxType.ACTION:
+			SignalBus.connect("display_action", _on_display_dialog)
+	SignalBus.connect("hide_dialog", _on_hide_dialog)
+
+func _on_display_dialog(text_key: String):
+	$Label.text = dialog_dict[text_key]
 	show()
-	$Label.text = dialog
+	
+func _on_hide_dialog():
+	hide()
 
