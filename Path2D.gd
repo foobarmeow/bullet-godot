efe0997f69ac42e14b44335b86b5ebd5fab805c5
foobarmeow <caleb.aaron.davis@gmail.com>
2023-12-27 12:52:23 -0700

    We're getting there

--- Path2D.gd
+++ Path2D.gd
@@ -4,19 +4,21 @@ extends Line2D
 @export var point_texture: Texture
 @export var do = false:
 	set(value):
-		draw_items()
+		draw_items(value)
 		do = value
 	get: 
 		return do
 
 func _ready():
-	draw_items()
+	draw_items(true)
 
 # Called when the node enters the scene tree for the first time.
-func draw_items():
+func draw_items(draw: bool):
 	for c in $Spawn.get_children():
 		c.queue_free()
 		
+	if !draw:
+		return
 	var last_point
 	for p in points:
 		if last_point == null:
@@ -25,21 +27,24 @@ func draw_items():
 			
 		var segment = p - last_point
 		var points_in_segment = floor(segment.length() / point_texture.get_width())
-		
+		print(points_in_segment)
 		for i in points_in_segment:
-			var t = i / (points_in_segment - 1)
-			print("t", t)
+			var t: float
+			if points_in_segment == 1:
+				t = 1
+			else:
+				t = i / (points_in_segment - 1)
+			print(t)
 			var np = last_point.lerp(p, t)
-					
-			var s = Sprite2D.new()
-			s.texture = point_texture
-			s.position = np
+			print(np)
+			set_at(np, segment.angle())
 			
-			$Spawn.add_child(s)
-			s.set_owner(get_tree().get_edited_scene_root())
-			
-		
 		last_point = p
 		
-		
-	
+func set_at(p: Vector2, angle: float):
+	var l = PointLight2D.new()
+	l.texture = point_texture
+	l.position = p
+	l.rotation = angle
+	$Spawn.add_child(l)
+	l.set_owner(get_tree().get_edited_scene_root())
